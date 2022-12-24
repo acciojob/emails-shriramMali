a8060c110db2de0fcbdbcaa3ebf7e3e280cf2d9d
malishrirama17@gmail.com <you@example.com>
2022-12-24 17:44:36 +0530

    cc

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -38,36 +38,7 @@ public class Email {
                     newPassword.matches(".*[^A-Za-z0-9].*")){
                 setPassword(newPassword);
                 System.out.println("Password changed successfully");
-            }else{
-                System.out.println(" Please try again.");
             }
-        }else{
-            System.out.println("Please try again.");
         }
-
     }
 }
-   /* public void changePassword(String oldPassword, String newPassword){
-        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
-        // 1. It contains at least 8 characters
-        // 2. It contains at least one uppercase letter
-        // 3. It contains at least one lowercase letter
-        // 4. It contains at least one digit
-        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-        if(this.password.equals(newPassword)) {
-            if(newPassword.length() >= 8 &&
-                    newPassword.matches(".*[A-Z].*") &&
-                    newPassword.matches(".*[a-z].*") &&
-                    newPassword.matches(".*[0-9].*") &&
-                    newPassword.matches(".*[^A-Za-z0-9].*")){
-                setPassword(newPassword);
-                System.out.println("Password changed successfully");
-            }else{
-                System.out.println("New password doesn't meet the required criteria. Please try again.");
-            }
-        }else{
-            System.out.println("Old password is incorrect. Please try again.");
-        }
-
-    }
-}*/
